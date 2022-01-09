658c064c4dce10a059adc864767ca8e72b34a228
Fede <federicous@gmail.com>
2022-01-09 00:41:10 -0300

    monogo first commit

---
+++ utils/firebase/index.js
@@ -0,0 +1,8 @@
+
+let admin = require("firebase-admin");
+
+let serviceAccount = require("path/to/serviceAccountKey.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount)
+});
