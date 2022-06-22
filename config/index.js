8d0ea75766122af51f4706355e375c95150862f5
Fede <federicous@gmail.com>
2022-06-22 16:55:13 -0300

    server implementado principal

--- config/index.js
+++ config/index.js
@@ -1,17 +1,6 @@
 require('dotenv').config()
 let MongoStore = require("connect-mongo");
 
-let whitelist = ['http://localhost', 'https://ecommerce-fede.herokuapp.com/']
-let corsOptions = {
-	origin: function (origin, callback) {
-		if (whitelist.indexOf(origin) !== -1) {
-			callback(null, true)
-		} else {
-			callback(new Error('Not allowed by CORS'))
-		}
-	}
-}
-
 const MONGO_ATLAS=process.env.MONGO_URL_ATLAS;
 const MONGO_SECRET=process.env.MONGO_SECRET;
 const COOKIE_AGE=process.env.COOKIE_AGE;
@@ -34,10 +23,8 @@ let sessionConfig = {
 const config = {
 	PORT: process.env.PORT || '8088',
 	MONGO_ATLAS: process.env.MONGO_URL_ATLAS,
-	CORS: "*",
+	CORS: process.env.CORS || "*",
 	SESSION: sessionConfig,
-
-
 }
 
 module.exports = {
--- index2.js
+++
@@ -1,3 +0,0 @@
-const Server = require('./server/server')
-const server = new Server();
-server.init()
