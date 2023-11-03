64430abd3c59d991d3fca3181310fe11c42375b8
Fede <federicous@gmail.com>
2023-11-03 01:12:29 -0300

    servidor https

--- config/index.js
+++ config/index.js
@@ -13,7 +13,7 @@ let CORS_CLIENT=["http://localhost:3000", "http://localhost:3001"];
 
 if (process.env.NODE_ENV === 'production') {
 	// CORS_CLIENT= "http://app.distribuidorabrmtools.com"
-	CORS_CLIENT= ["http://app.distribuidorabrmtools.com","http://app.distribuidorabrmtools.com:8081"]
+	CORS_CLIENT= ["http://app.distribuidorabrmtools.com","http://app.distribuidorabrmtools.com:8081","https://app.distribuidorabrmtools.com","https://app.distribuidorabrmtools.com:8081"]
       }
 
 console.log(CORS_CLIENT);
