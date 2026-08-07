bae6b448c7e038804d1397e7805a992160bad58d
Fede <federicous@gmail.com>
2026-08-07 19:37:12 -0300

    fix: desproteger endpoint GET /api/product para permitir detalle publico de productos

--- components/pruductos/index.js
+++ components/pruductos/index.js
@@ -5,8 +5,8 @@ const Autenticacion = require("../../middleware/autenticacion");
 module.exports = app => {
     const prodRouter = express.Router();
     app.use("/api/product", prodRouter);
-    prodRouter.get("/", Autenticacion.usuario ,productoController.getAllElement);
-    prodRouter.get("/:id", Autenticacion.usuario, productoController.getElement);
+    prodRouter.get("/", productoController.getAllElement);
+    prodRouter.get("/:id", productoController.getElement);
     prodRouter.post("/", Autenticacion.administrador, productoController.createElement);
     prodRouter.put("/:id", Autenticacion.administrador, productoController.updateElement);
     prodRouter.delete("/:id", Autenticacion.administrador, productoController.deleteElement);
